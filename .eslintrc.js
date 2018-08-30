592c1209ec3f1728458dd5bc79ae91d64870711e
guhy1 <guhy1@lenovo.com>
2018-08-30 11:30:31 +0800

    add print pdf funcition

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,18 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
+    "parser":"babel-eslint",
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 2016,
         "sourceType": "module"
     },
     "rules": {
+        "no-console":[
+            "off",{
+                "allow":["warn","error","info"]
+            }
+            ],
         "indent": [
             "error",
             4
