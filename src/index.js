58f1a906a2bc1da33c07ee51bbaab1e8832a37fd
guhy1 <guhy1@lenovo.com>
2018-09-11 11:15:35 +0800

    change dir for pdf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const  puppeteer = require('puppeteer');
 const  config = require('../config/config');
 const chalk = require('chalk');
-
 let main = async function (){
     const browser = await puppeteer.launch({
         headless:true,
@@ -10,7 +9,6 @@ let main = async function (){
     });
     const page = await browser.newPage();
     await page.goto('http://www.zhoujingen.cn/blog/wp-login.php');
-
     console.log(chalk.green('打开登录页面'));
     await page.setViewport({
         width:1440,
@@ -18,9 +16,9 @@ let main = async function (){
     });
     console.log(chalk.green('reset viewpoint'));
     await page.focus('#user_login');
-    await page.keyboard.sendCharacter('18210655521');
+    await page.keyboard.sendCharacter('909055521');
     await page.focus('#user_pass');
-    await page.keyboard.sendCharacter('mimamima');
+    await page.keyboard.sendCharacter('999999');
     await page.click('#wp-submit');
     console.log(chalk.green('登录成功'));
     await page.waitForSelector('#wp-admin-bar-site-name');
@@ -28,7 +26,6 @@ let main = async function (){
     await page.waitForSelector('#wp-admin-bar-view-site');
     await page.click('#wp-admin-bar-view-site');
     console.log(chalk.green('进入掌控你的生活主页'));
-    
     const exercisePage = await browser.newPage();
     await exercisePage.goto('http://www.zhoujingen.cn/blog/');
     await exercisePage.waitFor(200);
@@ -39,9 +36,7 @@ let main = async function (){
     await exercisePage.click('#menu-item-9931');
     console.log(chalk.green('进入手头心练习主页'));
     await exercisePage.waitFor(1000);  
-
     let aTags = await exercisePage.evaluate(()=>{
-       
         let as = [...document.querySelectorAll('h2.entry-title a')];
         return as.map((a)=>{
             return {
@@ -51,44 +46,28 @@ let main = async function (){
         }); 
     });
     console.log(aTags);
-
-    
-
     await exercisePage.click('.nextpostslink');
     console.log(chalk.green('进入练习的第二页'));
     await exercisePage.waitFor(1000);
-
-    let aTagsSecond = await exercisePage.evaluate(()=>{
-       
+    let aTagsSecond = await exercisePage.evaluate(()=>{  
         let as = [...document.querySelectorAll('h2.entry-title a')];
         return as.map((a)=>{
             return {
                 href:a.href.trim(),
                 name:a.text
             };
-
-            // aTags.push({
-            //     href:a.href.trim(),
-            //     name:a.text
-            // });
         }); 
     });
-
     let allLinks=aTags.concat(aTagsSecond);
     console.log(chalk.green(allLinks));
-    //page.close();
-   
-
     for (let i = 0; i < allLinks.length; i++) {
         let exercisePage = await browser.newPage();
         let a = allLinks[i];
         await exercisePage.goto(a.href);
         await exercisePage.waitFor(2000);
         await exercisePage.pdf({path: `${config.fileDir}/${i}.pdf`});
-       
     }
     browser.close();
 };
-
 main();
 
--- src/taobaoCrepper.js
+++ src/taobaoCrepper.js
@@ -10,48 +10,104 @@ let main = async function(){
 
     let page = await browser.newPage();
 
-    page.on('console',msg =>{
-        if(typeof msg === 'object'){
-            console.dir(msg);
-        }else{
-            console.log(chalk.yellow(msg));
-        }
-    });
+    // page.on('console',msg =>{
+    //     if(typeof msg === 'object'){
+    //         console.dir(msg);
+    //     }else{
+    //         console.log(chalk.yellow(msg));
+    //     }
+    // });
     await page.goto(url,{
         waitUntil:'networkidle0'
     });
 
     console.log(chalk.green('进入宝贝详情页'));
 
-    await page.setViewport({
-        width:1920,
-        height:1080
-    });
-
-    // let title = await page.$eval('.tb-detail-hd > h1 >a',ele => ele.text);
-    // console.log(chalk.green(title));
-    let price = await page.$eval('.tm-price',ele => ele.text);
-    console.log(chalk.green(price));
-
-
-
-
-    
-    
-    // let page = await page.evaluate(()=>{
- 
-         
-
-
+    // await page.setViewport({
+    //     width:1920,
+    //     height:1080
     // });
 
+    let obj ={
+        title:'',
+        colors:[],
+        price:0,
+        suit:[],
+        skus:[],
+        detailImgs:[],
+        evaList:[]
 
+    
+    };
+    try{
+
+        
+        await page.click('#sufei-dialog-close');
+        // page.on('dialog',async ()=>{
+        //     
+        //     console.log(chalk.green('关闭模态框'));
+        // });
+
+        await page.waitFor(15000);
+
+        obj.title= await page.$eval('.tb-detail-hd > h1 >a',ele =>ele.text) ;
+        obj.price = await page.$eval('.tm-price',ele => ele.innerHTML);
+    
+        obj.colors = await page.evaluate(()=>{  
+            let as = [...document.querySelectorAll('li.tb-txt a > span')];
+            return as.map((a)=>{
+                return a.innerHTML;
+            }); 
+        });
+
+        obj.suit = await page.evaluate(()=>{  
+            let as = [...document.querySelectorAll('li.tb-selected > a > span')];
+            return as.map((a)=>{
+                return a.innerHTML;
+            }); 
+        });
+
+        obj.skus = await page.evaluate(()=>{  
+            let as = [...document.querySelectorAll('ul#J_UlThumb >li> a > img')];
+            return as.map((a)=>{
+                return a.src;
+            }); 
+        });
 
     
 
-     
+        obj.detailImgs = await page.evaluate(()=>{  
+            let as = [...document.querySelectorAll('div.ke-post > p > img')];
+            return as.map((a)=>{
+                return a.src;
+            }); 
+        });
+        //console.log(obj);
 
 
+
+        //await page.waitForSelector('.J_TabBar');
+
+        
+   
+        await page.click('a[href="#J_Reviews"] ');
+        
+
+
+        obj.evaList = await page.evaluate(()=>{  
+            console.log('进入评价分析抓取方法');
+            let evaList = [...document.querySelectorAll('td.tm-col-master>div.tm-rate-content>div.tm-rate-fulltxt')];
+            return evaList.map((a)=>{
+                console.log(a);
+                return a.text;
+            }); 
+        });
+
+        console.log(obj);
+    }catch(err){
+        console.log(chalk.red(err));
+    }
+    
 };
 
 main();
